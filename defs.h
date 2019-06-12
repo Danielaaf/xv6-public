545ee875ad56aa5de95ce2d5a96e20d750cfb0b5
Danielaaf <41976236+Danielaaf@users.noreply.github.com>
2019-06-12 17:31:12 -0400

    probando la tarea3

--- defs.h
+++ defs.h
@@ -121,7 +121,7 @@ int             wait(void);
 void            wakeup(void*);
 void            yield(void);
 int				getprocs(void);
-int				addrtranslate(char*)
+int				addrtranslate(char*);
 
 // swtch.S
 void            swtch(struct context**, struct context*);
