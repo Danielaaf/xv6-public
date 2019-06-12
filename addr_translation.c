415f45c207ff1ef5dd7f84f4e1b4bd90d9a92bad
Danielaaf <41976236+Danielaaf@users.noreply.github.com>
2019-06-12 15:29:05 -0400

    probando la tarea3

--- addr_translation.c
+++ addr_translation.c
@@ -7,12 +7,12 @@
 #include "user.h"
 
 int main(int argc, char *argv[]) {
-	//printf(1, "\n ----- ADDRESS TRANSLATION -----\n");
-	//char *virtual_addr;
-	//virtual_addr = argv[1];
+	printf(1, "\n ----- ADDRESS TRANSLATION -----\n");
+	char *vaddr;
+	vaddr = argv[1];
 
-	//printf(1, "\n Entered virtual adress: %s\n", virtual_addr);
-	//printf(1, "\n Status from system call: %s", addr_translation(virtual_addr));
-	addr_translate();
+	printf(1, "\n Entered virtual adress: %s\n", vaddr);
+	printf(1, "\n Status from system call: %s", addr_translate(vaddr));
+	
 	exit();
 }
